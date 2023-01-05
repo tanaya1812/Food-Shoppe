01b3129ff31073cd94152baa7741ee4fb7cf28b4
tanaya1812 <79663392+tanaya1812@users.noreply.github.com>
2023-01-05 14:38:13 +0530

    first commit

---
+++ components/footer.js
@@ -0,0 +1,18 @@
+export const footer = () => {
+    return (
+        `<div id="footer">
+            <p class="text-center">Made with ❤️ by Tanaya Raikwar</p>
+            <div>
+                <button class="btn btn-primary">
+                    <a href="https://www.linkedin.com/in/tanaya-raikwar/" target="_blank">LinkedIn</a>
+                </button>
+                <button class="btn btn-success">
+                    <a href="https://github.com/tanaya1812" target="_blank">GitHub</a>
+                </button>
+                <button class="btn btn-danger">
+                    <a href=" " target="_blank">Portfolio</a>
+                </button>
+            </div>
+        </div>`
+    );
+};
